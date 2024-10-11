05e8779ca197b0e765492434f8bc743fe33c9be2
Ivan Toro <ivantoro.p@gmail.com>
2024-10-11 20:05:11 -0300

    Primer Pre-Entrega Proyecto Final

---
+++ src/routes/users.router.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { UserControllers } from "../controllers/users.controller.js";
+
+const usersController = new UserControllers();
+const router = Router();
+
+router.get("/", usersController.getAllUsers);
+router.get("/mock", usersController.createUserMock);
+
+router.get("/:uid", usersController.getUser);
+router.put("/:uid", usersController.updateUser);
+router.delete("/:uid", usersController.deleteUser);
+router.delete("/clearUsers/delete", usersController.deleteAllUser);
+
+export default router;
